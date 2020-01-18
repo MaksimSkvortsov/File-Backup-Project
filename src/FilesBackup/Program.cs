8d73a2053a57483ff69ae505194bc8a89b8ab867
mskvortsov <mskvortsov@acr.org>
2020-01-17 19:38:42 -0500

    Initial

---
+++ src/FilesBackup/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FilesBackup
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Files backup app");
+        }
+    }
+}
