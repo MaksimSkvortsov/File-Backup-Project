cbed72c4ddc521c33f73a64961468e7888c0a9f1
mskvortsov <mskvortsov@acr.org>
2020-01-20 18:17:51 -0500

    Add file system helper to the unit tests project

---
+++ tests/FilesBackup.Tests/TempStorage.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.IO;
+
+namespace FilesBackup.Tests
+{
+    class TempStorage : IDisposable
+    {
+        public string StoragePath { get; }
+
+
+        public TempStorage()
+        {
+            var currentDirectory = Path.GetTempPath();
+            var storageName = "FileBackupTest" + Guid.NewGuid().ToString();
+
+            StoragePath = Path.Combine(currentDirectory, storageName);
+            Directory.CreateDirectory(StoragePath);
+        }
+
+
+        public void AddFolder(string name)
+        {
+            var directoryPath = Path.Combine(StoragePath, name);
+
+            if (Directory.Exists(directoryPath))
+                throw new ArgumentException("directory already exists");
+
+            Directory.CreateDirectory(directoryPath);
+        }
+
+        public void AddFile(string name)
+        {
+            var filePath = Path.Combine(StoragePath, name);
+
+            if (File.Exists(filePath))
+                throw new ArgumentException("file already exists");
+
+            var fileDirectory = Directory.GetParent(filePath).FullName;
+            if (!Directory.Exists(fileDirectory))
+            {
+                Directory.CreateDirectory(fileDirectory);
+            }
+
+            using (var fileStream = File.Create(filePath)) { }
+        }
+
+        public bool FileExists(string relativeFilePath)
+        {
+            return File.Exists(Path.Combine(StoragePath, relativeFilePath));
+        }
+
+        public void DeleteStorage()
+        {
+            if (Directory.Exists(StoragePath))
+            {
+                Directory.Delete(StoragePath, true);
+            }
+        }
+
+        public void Dispose()
+        {
+            DeleteStorage();
+        }
+    }
+}
