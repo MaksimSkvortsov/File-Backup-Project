2759af31ad3c99b0166a724b953fd40fdef8f2b8
mskvortsov <mskvortsov@acr.org>
2020-01-20 18:19:53 -0500

    Add path extensions to simplify work with system paths

---
+++ src/FilesBackup/Utils/PathExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace FilesBackup.Utils
+{
+    public class PathExtensions
+    {
+        public static string GetRelativePath(string fullPath, string root)
+        {
+            Assert.NotNull(fullPath, nameof(fullPath));
+            Assert.NotNull(root, nameof(root));
+
+            if (fullPath.Length < root.Length)
+                throw new ArgumentException($"{nameof(fullPath)} cannot be shorter than {nameof(root)}");
+
+            if(root.Length < 3)
+            {
+                throw new ArgumentException($"{nameof(root)} value \"{root}\" is too short. Minimum length is 3 characters");
+            }
+
+            //ToDo: consider adding root is part of fullPath verification
+
+            if (fullPath.Length == root.Length)
+                return string.Empty;
+
+            var startIndex = root.Length == 3 ? root.Length : root.Length + 1;
+
+            return fullPath.Substring(startIndex, fullPath.Length - startIndex);
+        }
+    }
+}
---
+++ tests/FilesBackup.Tests/Utils/PathExtensionsTests.cs
@@ -0,0 +1,29 @@
+﻿using FilesBackup.Utils;
+using System;
+using Xunit;
+
+namespace FilesBackup.Tests.Utils
+{
+    public class PathExtensionsTests
+    {
+        [Theory]
+        [InlineData(@"C:\Folder\Inner folder", @"C:\Folder\Inner folder", "")]
+        [InlineData(@"C:\Folder\Inner folder\file.txt", @"C:\Folder", @"Inner folder\file.txt")]
+        [InlineData(@"C:\Folder\Inner folder\file.txt", @"C:\Folder\Inner folder", @"file.txt")]
+        [InlineData(@"C:\Folder\Inner folder\file.txt", @"C:\", @"Folder\Inner folder\file.txt")]
+        public void GetRelativePath_ValidInput(string fullPath, string root, string expectedRelativePath)
+        {
+            var result = PathExtensions.GetRelativePath(fullPath, root);
+
+            Assert.Equal(expectedRelativePath, result);
+        }
+
+        [Theory]
+        [InlineData(@"C:\Folder\Inner folder\file.txt", @"C:")]
+        [InlineData(@"C:\Folder\Inner folder", @"C:\Folder\Inner folder\file.txt")]
+        public void GetRelativePath_InvalidInput(string fullPath, string root)
+        {
+            Assert.Throws<ArgumentException>(() => PathExtensions.GetRelativePath(fullPath, root));
+        }
+    }
+}
